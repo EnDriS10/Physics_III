65f201c2d3ec885536734bc1710c94e22536d741
César Acosta <cesarendris@gmail.com>
2026-03-30 09:59:27 +0200

    Add new file A2a_partc_pozo_potencial_finito.m

--- QM_1/A2a_partc_pozo_potencial_finito.m
+++ QM_1/A2a_partc_pozo_potencial_finito.m
@@ -1,8 +1,15 @@
-clear; clc;
+
+
+%% Particula en un Pozo de Potencial Finito
+% 
+%  Entrega: César Endris Acosta Martínez— Problema 2
+% =========================================================================
+
+clear; close all; clc;
 
 %% DATOS
-m=9.109E-31;
-h=6.626E-34;
+m=9.109383701E-31;
+h=6.62607015E-34;
 h_ = h / (2 * pi);
 
 V0=1;
@@ -17,7 +24,7 @@ p1= @(x) x.*tan(x) - ( (var^2) - x.^2 ).^0.5;
 x=0:dx:2*pi;
 [ep1]=mi_zeros_autom(x,p1(x));
 
-ep1=ep1( abs(cos(ep1)) > dx  & ((var^2) - ep1.^2 ) > 0 );
+ep1=ep1( abs(cos(ep1)) > dx  & ((var^2) - ep1.^2 ) > 0 ); % filtrar los ceros que vienen de errores como dividir entre 0 e imaginarios
 E1 = V0.*(ep1./var).^2;
 
 alf1= sqrt( (2.*m.*(V0-E1))./(h_^2));
@@ -28,7 +35,7 @@ k1= sqrt( (2.*m.*E1)./(h_^2));
 p2= @(x) x.* cot(x) + ( (var^2) - x.^2 ).^0.5;
 [ep2]=mi_zeros_autom(x,p2(x));
 
-ep2=ep2( abs(sin(ep2)) > dx  & ((var^2) - ep2.^2 ) > 0 );
+ep2=ep2( abs(sin(ep2)) > dx  & ((var^2) - ep2.^2 ) > 0 ); % filtrar los ceros que vienen de errores como dividir entre 0 e imaginarios
 E2 = V0.*(ep2./var).^2;
 
 alf2= sqrt( (2.*m.*(V0-E2))./(h_^2));
@@ -123,6 +130,7 @@ grid on
 xlabel('Posición (x/L)');
 ylabel('Wave Function \psi \cdot \surdL');
 title('Wave Functions');
+xlim([-2 2]);
 legend( ...
     [h1(1), h2(1), h3(1), xl1], ...
     {['\psi_1 (par, E_1/V_0 = '  + string(round(E1(1)/V0, 3)) + ')'], ...
@@ -140,16 +148,18 @@ plot(x_1./L, y1_1.*sqrt(L), 'r')
 plot(x_2./L, y1_2.*sqrt(L), 'r')
 plot(x_3./L, y1_3.*sqrt(L), 'r')
 xline(-0.5, 'k--'); xline(0.5, 'k--');
+xlim([-1.5 1.5]);
 title('\psi_1 (par)')
 xlabel('x/L'); ylabel('\psi \cdot \surdL')
 grid on; hold off
 
 subplot(3,2,2)
 hold on
-plot(x_1./L, (y1_1.*sqrt(L)).^2, 'r')
-plot(x_2./L, (y1_2.*sqrt(L)).^2, 'r')
-plot(x_3./L, (y1_3.*sqrt(L)).^2, 'r')
+plot(x_1./L, pdf1_1.*(L), 'r')
+plot(x_2./L, pdf1_2.*(L), 'r')
+plot(x_3./L, pdf1_3.*(L), 'r')
 xline(-0.5, 'k--'); xline(0.5, 'k--');
+xlim([-1.5 1.5]);
 title('|\psi_1|^2 (par)')
 xlabel('x/L'); ylabel('|\psi|^2 \cdot L')
 grid on; hold off
@@ -161,16 +171,18 @@ plot(x_1./L, y2_1.*sqrt(L), 'b')
 plot(x_2./L, y2_2.*sqrt(L), 'b')
 plot(x_3./L, y2_3.*sqrt(L), 'b')
 xline(-0.5, 'k--'); xline(0.5, 'k--');
+xlim([-1.5 1.5]);
 title('\psi_2 (impar)')
 xlabel('x/L'); ylabel('\psi \cdot \surdL')
 grid on; hold off
 
 subplot(3,2,4)
 hold on
-plot(x_1./L, (y2_1.*sqrt(L)).^2, 'b')
-plot(x_2./L, (y2_2.*sqrt(L)).^2, 'b')
-plot(x_3./L, (y2_3.*sqrt(L)).^2, 'b')
+plot(x_1./L, pdf2_1.*(L), 'b')
+plot(x_2./L, pdf2_2.*(L), 'b')
+plot(x_3./L, pdf2_3.*(L), 'b')
 xline(-0.5, 'k--'); xline(0.5, 'k--');
+xlim([-1.5 1.5]);
 title('|\psi_2|^2 (impar)')
 xlabel('x/L'); ylabel('|\psi|^2 \cdot L')
 grid on; hold off
@@ -182,16 +194,18 @@ plot(x_1./L, y3_1.*sqrt(L), 'g')
 plot(x_2./L, y3_2.*sqrt(L), 'g')
 plot(x_3./L, y3_3.*sqrt(L), 'g')
 xline(-0.5, 'k--'); xline(0.5, 'k--');
+xlim([-1.5 1.5]);
 title('\psi_3 (par)')
 xlabel('x/L'); ylabel('\psi \cdot \surdL')
 grid on; hold off
 
 subplot(3,2,6)
 hold on
-plot(x_1./L, (y3_1.*sqrt(L)).^2, 'g')
-plot(x_2./L, (y3_2.*sqrt(L)).^2, 'g')
-plot(x_3./L, (y3_3.*sqrt(L)).^2, 'g')
+plot(x_1./L, pdf3_1.*(L), 'g')
+plot(x_2./L, pdf3_2.*(L), 'g')
+plot(x_3./L, pdf3_3.*(L), 'g')
 xline(-0.5, 'k--'); xline(0.5, 'k--');
+xlim([-1.5 1.5]);
 title('|\psi_3|^2 (par)')
 xlabel('x/L'); ylabel('|\psi|^2 \cdot L')
 grid on; hold off
@@ -218,7 +232,7 @@ for i = 1:length(E)
 end
 
 % --- Formato ---
-xlim([-3, 3])
+xlim([-1, 1])
 ylim([-0.05, 1.2])
 xlabel('x/L')
 ylabel('V / V_0')
@@ -248,3 +262,4 @@ Int_def=y0 + Int_def ;
 Int_Indef=y0 + Int_Indef;
 
 end
+
